4e9d4de2aa2854a81855e365bf80b57e6d5b4c36
Will Laeri <wlaeri@gmail.com>
2020-02-18 14:20:44 -0500

    tests include correct url generation

--- tests/BobRossClient.test.js
+++ tests/BobRossClient.test.js
@@ -26,6 +26,16 @@ test('getUrl method returns a string', () => {
   expect(typeof client.getUrl('abc123')).toBe('string')
 })
 
+test('getUrl string is correct', () => {
+  const serverUrl = 'https://knotel.io/images'
+  const hmacs = { 'S300x225*W0seO': '2fdbb7f8f2c8ecf49ac437a40b25950ff8c930e9' }
+  const client = new BobRossClient({ serverUrl, hmacs })
+  const hash = '256cd9a0e3824a9b81e9f41e616019e8'
+  const options = { resize: "300x225*" }
+  const url = 'https://knotel.io/images/H2fdbb7f8f2c8ecf49ac437a40b25950ff8c930e9S300x225*W0seO/256cd9a0e3824a9b81e9f41e616019e8'
+  expect(client.getUrl(hash, options)).toBe(url)
+})
+
 test('getSrcset method fails without a hash param', () => {
   expect.assertions(1)
   try {
